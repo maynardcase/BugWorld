ee3b36d95d540fe7ee9dec022e48abb100e9cc82
maynard <maynard.case@gmail.com>
2014-08-20 22:07:15 +0100

    Avoid unnecessary local variable

--- src/com/maynardcase/bugworld/Random.java
+++ src/com/maynardcase/bugworld/Random.java
@@ -21,8 +21,6 @@ public class Random {
     public static int randInt(int min, int max) {
         // nextInt is normally exclusive of the top value,
         // so add 1 to make it inclusive
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-
-        return randomNum;
+        return rand.nextInt((max - min) + 1) + min;
     }
 }
